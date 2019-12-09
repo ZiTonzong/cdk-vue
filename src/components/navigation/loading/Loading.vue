326d55888eeb479167ebf41bac035bbb7e4cc1df
ChengDakang <715167986@qq.com>
2019-12-09 16:54:05 +0800

    初始化loading组件

---
+++ src/components/navigation/loading/Loading.vue
@@ -0,0 +1,12 @@
+<template>
+  <transition name="loading-fade">
+		<div class="c-loading" v-show="visible" :style="elStyle">
+			<div class="progress" ref="proress" :style="proStyle"></div>
+		</div>
+	</transition>
+</template>
+<script>
+export default {
+	
+}
+</script>
